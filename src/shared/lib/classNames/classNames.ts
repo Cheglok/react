115619aa5186b2c54ce06c4d563368ef93f80c2a
Cheglok <cheglok37@gmail.com>
2024-11-02 10:29:52 +0500

    did lesson10

---
+++ src/shared/lib/classNames/classNames.ts
@@ -0,0 +1,10 @@
+type Mods = Record<string, string | boolean>
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+    return [
+        cls,
+        ...additional,
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
+            .map(([className, _]) => className)
+    ].join(" ");
+}

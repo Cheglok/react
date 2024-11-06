4f944e31792e3a85a4c7967d18af63f8ae039e71
Cheglok <cheglok37@gmail.com>
2024-11-06 22:10:55 +0500

    did lesson20 add jest

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,19 +1,27 @@
-import {classNames} from "./classNames";
+import { classNames } from './classNames';
 
-describe("classNames", () => {
-    test("test", () => {
-        expect(classNames("someClass")).toBe("someClass");
+describe('classNames', () => {
+    test('test', () => {
+        expect(classNames('someClass')).toBe('someClass');
     });
 
-    test("test", () => {
-        expect(classNames("someClass", {}, ["2", "3"])).toBe("someClass 2 3");
+    test('test', () => {
+        expect(classNames('someClass', {}, ['2', '3'])).toBe('someClass 2 3');
     });
 
-    test("test", () => {
-        expect(classNames("someClass", {hovered: true, scrollable: true}, ["2", "3"])).toBe("someClass 2 3 hovered scrollable");
-    })
+    test('test', () => {
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+            ['2', '3'],
+        )).toBe('someClass 2 3 hovered scrollable');
+    });
 
-    test("test", () => {
-        expect(classNames("someClass", {hovered: true, scrollable: false}, ["2", "3"])).toBe("someClass 2 3 hovered");
-    })
-})
+    test('test', () => {
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: false },
+            ['2', '3'],
+        )).toBe('someClass 2 3 hovered');
+    });
+});
